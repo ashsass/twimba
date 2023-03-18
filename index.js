280da03a38b2f451e5d72b269dabf366dbfd9892
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-03-18 11:40:27 -0400

    Added a render function to render the twimba feed

--- index.js
+++ index.js
@@ -5,3 +5,39 @@ const tweetInput = document.getElementById('tweet-input')
 tweetBtn.addEventListener('click', function() {
     console.log(tweetInput.value)
 })
+
+function getFeedHtml(tweets) {
+    let feedHTML = ``
+    tweets.forEach(function(tweet) {
+        feedHTML += `
+            <div class="tweet">
+                <div class="tweet-inner">
+                    <img src="${tweet.profilePic}" class="profile-pic">
+                    <div>
+                        <p class="handle">${tweet.handle}</p>
+                        <p class="tweet-text">${tweet.tweetText}</p>
+                        <div class="tweet-details">
+                            <span class="tweet-detail">
+                                ${tweet.replies.length}
+                            </span>
+                            <span class="tweet-detail">
+                                ${tweet.likes}
+                            </span>
+                            <span class="tweet-detail">
+                                ${tweet.retweets}
+                            </span>
+                        </div>   
+                    </div>            
+                </div>
+            </div>`
+    })
+
+    return feedHTML;
+}
+    
+
+function render() {
+    document.getElementById('feed').innerHTML = getFeedHtml(tweetsData)
+}
+
+render()
