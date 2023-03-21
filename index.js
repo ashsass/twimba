84fc5a83e65cf80a8f20a7cc1e6d27585150f618
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-03-21 13:37:59 -0400

    Added a function to grab the uuid of the target object and increment the likes of that object

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
-import { tweetsData } from "./data.js";
-const tweetBtn = document.getElementById('tweet-btn')
+import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
-tweetBtn.addEventListener('click', function() {
+tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
-/*
-fa-regular, 
-    fa-solid, 
-    fa-comment-dots, 
-    fa-heart, 
-    fa-retweet
-    */
 
-function getFeedHtml(tweets) {
-    let feedHTML = ``
-    tweets.forEach(function(tweet) {
-        feedHTML += `
-            <div class="tweet">
-                <div class="tweet-inner">
-                    <img src="${tweet.profilePic}" class="profile-pic">
-                    <div>
-                        <p class="handle">${tweet.handle}</p>
-                        <p class="tweet-text">${tweet.tweetText}</p>
-                        <div class="tweet-details">
-                            <span class="tweet-detail">
-                                <i class="fa-regular fa-comment-dots"></i>
-                                ${tweet.replies.length}
-                            </span>
-                            <span class="tweet-detail">
-                                <i class="fa-solid fa-heart"></i>
-                                ${tweet.likes}
-                            </span>
-                            <span class="tweet-detail">
-                                <i class="fa-solid fa-retweet"></i>
-                                ${tweet.retweets}
-                            </span>
-                        </div>   
-                    </div>            
-                </div>
-            </div>`
+//Take the uuid of the tweet that is being clicked on by identifying the data attribute
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+})
+
+//When the data attribute it grabbed, check which object matches the uuid and increment the likes
+function handleLikeClick(tweetId){
+    tweetsData.forEach(item => {
+        if(item.uuid === tweetId){
+            const targetTweetObj = item //Saving the object that matches the uuid
+            targetTweetObj.likes++
+            console.log(targetTweetObj)
+        }
     })
-
-    return feedHTML;
 }
-    
 
-function render() {
-    document.getElementById('feed').innerHTML = getFeedHtml(tweetsData)
+function getFeedHtml(){
+    let feedHtml = ``
+    
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
 }
 
 render()
