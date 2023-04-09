419299192ac09b9becc4e67975a736628bac5708
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-04-09 10:49:32 -0400

    Cleaned code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { tweetsData } from './data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-const tweetInput = document.getElementById('tweet-input')
 
 
 //Take the uuid of the tweet that is being clicked on by identifying the data attribute
@@ -45,16 +44,21 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick() {
-    tweetsData.unshift({
-        handle: `@Scrimba`,
-        profilePic: `scrimbalogo.png`,
-        likes: 0,
-        retweets: 0,
-        tweetText: tweetInput.value,
-        replies: [],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: uuidv4()})
+    const tweetInput = document.getElementById('tweet-input')
+    if(tweetInput.value){
+        tweetsData.unshift({
+            handle: `@Scrimba`,
+            profilePic: `images/scrimbalogo.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()})
+        render()
+        tweetInput.value = ''
+    }
 }
 
 function getFeedHtml(){
